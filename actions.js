c116e94878ee3fdbf36fcd699eac1c2ca953411d
sergioandresserrano <sergio.serrano.avellaneda@gmail.com>
2025-11-12 14:43:09 -0500

    Add files via upload

--- actions.js
+++ actions.js
@@ -1,31 +1,24 @@
 $(window).load(function () {
 
     setTimeout(() => {
-        $(".loader").fadeOut()
-    }, 500);
+        $(".loader_container").fadeOut()
+    }, 200);
 
 
-    //gsap.to('.circulo', { rotation: 50, duration: 1, repeat: -1, ease: "linear" })
+    //gsap.to('.circulo', { rotation: 50, duration: 1, repeat: -1, ease: "sine.inOut()" })
 
-    gsap.to('.logo1', { "top": "20px", duration: 1, delay: 1, ease: "sine.inOut()" })
-    gsap.from('.caja', { y: 50, opacity: 0, duration: 0.5, delay: 1.5, ease: "sine.inOut()" })
-    gsap.from('.caja_boton', { y: 50, opacity: 0, duration: 0.5, delay: 1.5, ease: "sine.inOut()" })
-    gsap.from('.footer', { y: 50, opacity: 0, duration: 0.5, delay: 1.5, ease: "sine.inOut()" })
-    gsap.from('.logos', { y: 20, opacity: 0, duration: 0.5, delay: 1.5, stagger: 0.15, ease: "sine.inOut()" })
-
-    gsap.from('.subtitulo', { y: 50, opacity: 0, duration: 0.5, delay: 0.3, ease: "sine.inOut()" })
-    gsap.from('.texto', { y: 50, opacity: 0, duration: 0.5, delay: 0.4, ease: "sine.inOut()" })
-    gsap.from('.boton', { x: 50, opacity: 0, duration: 0.5, delay: 0.4, stagger: 0.1, ease: "sine.inOut()" })
-
-    gsap.from('.mapas', { opacity: 0, duration: 0.5, delay: 0.1, ease: "sine.inOut()" })
-    gsap.from('.pin', { y: -50, opacity: 0, duration: 0.6, delay: 0.5, stagger: 0.15, ease: "elastic.out(1,0.6)" })
-
+    gsap.from('.logo', { y: -100, opacity: 0, duration: 0.8, delay: 0.6, ease: "elastic.out(1,0.6)" })
+    gsap.from('.logoalcaldia', { y: -100, opacity: 0, duration: 0.6, ease: "sine.inOut()" })
+    gsap.from('.caja', { y: -100, opacity: 0, duration: 0.4, delay: 0.6, ease: "sine.inOut()" })
+    gsap.from('.titulo', { y: -80, opacity: 0, duration: 1, delay: 0.7, ease: "elastic.out(1,0.5)" })
+    gsap.from('.boton', { y: -80, opacity: 0, duration: 1, delay: 0.8, ease: "elastic.out(1,0.5)" })
+    gsap.from('.fondoverde', { y: 200, duration: 0.8, ease: "sine.inOut()" })
 
     //BUCARAMANGA
-    $(".caja_boton").hover(function () {
-        gsap.to(this, { scale: 1.05, "background": "#ffbc07", duration: 1, ease: "elastic.out(1,0.5)" })
+    $(".boton").hover(function () {
+        gsap.to(this, { "background": "#30622A", "color": "#FFF", duration: 1, ease: "elastic.out(1,0.5)" })
     }, function () {
-        gsap.to(this, { scale: 1, "background": "#ff880e", duration: 1, ease: "elastic.out(1,0.5)" })
+        gsap.to(this, { "background": "#FFF", "color": "#30622A", duration: 1, ease: "elastic.out(1,0.5)" })
     });
 
 
